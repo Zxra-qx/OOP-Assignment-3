d8b4791b2f1cfe81542d47c791e6c4199c0b8d48
Zara Qadri <zaraqad@gmail.com>
2026-03-30 23:53:13 -0600

    Update SL.java

--- SL.java
+++ SL.java
@@ -1,79 +1,18 @@
 package sait.sll.utility;
 
-public class SLL implements LinkedListADT {
+import java.io.Serializable;
+
+public class SLL implements LinkedListADT, Serializable {
+    private static final long serialVersionUID = 1L;
 
     private Node head;
     private Node tail;
     private int size;
 
-    // Roselle
-
-    public void append(Object data) {
-        Node newNode = new Node(data);
-        if(head == null) {
-            head = tail = newNode;
-        }
-        else {
-            tail.setNext(newNode);
-            tail = newNode;
-        }
-        size++;
-    }//append
-
-    public void prepend(Object data) {
-        Node newNode = new Node(data);
-        if(head == null) {
-            head = tail = newNode;
-        }
-        else {
-            newNode.setNext(head);
-            head = newNode;
-        }
-        size++;
-    }//prepend
-
     @Override
-    public void delete(int index) throws IndexOutOfBoundsException {
-        if(index < 0 || index >= size)
-            throw new IndexOutOfBoundsException("Index for remove is out of range");
-
-        if(index == 0) { // removing first
-            head = head.getNext();
-            if(head == null) {
-                tail = null;
-            }
-        } else {
-            Node current = head;
-            int count = 0;
-
-            while(count < index - 1) {
-                count++;
-                current = current.getNext();
-            }
-
-            if(index == size - 1) {
-                current.setNext(null);
-                tail = current;
-            } else {
-                current.setNext(current.getNext().getNext());
-            }
-        }
-
-        size--;
-    }//delete
-
-    @Override
-    public int size() {
-        int count = 0;
-        Node current = head;
-        while(current != null) {
-            count++;
-            current = current.getNext();        
-        }
-        return count;
-    }//size
-
-    // Zara
+    public boolean isEmpty() {
+        return size == 0;
+    }
 
     @Override
     public void clear() {
@@ -83,14 +22,73 @@ public class SLL implements LinkedListADT {
     }
 
     @Override
-    public void replace(Object data, int index) throws IndexOutOfBoundsException {
-        if(index < 0 || index >= size)
+    public void append(Object data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+
+        size++;
+    }
+
+    @Override
+    public void prepend(Object data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            newNode.setNext(head);
+            head = newNode;
+        }
+
+        size++;
+    }
+
+    @Override
+    public void insert(Object data, int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Index out of range");
+        }
+
+        if (index == 0) {
+            prepend(data);
+            return;
+        }
+
+        if (index == size) {
+            append(data);
+            return;
+        }
+
+        Node newNode = new Node(data);
+        Node current = head;
+        int count = 0;
+
+        while (count < index - 1) {
+            current = current.getNext();
+            count++;
+        }
+
+        newNode.setNext(current.getNext());
+        current.setNext(newNode);
+        size++;
+    }
+
+    @Override
+    public void replace(Object data, int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of range");
+        }
 
         Node current = head;
         int count = 0;
 
-        while(count < index) {
+        while (count < index) {
             current = current.getNext();
             count++;
         }
@@ -99,14 +97,52 @@ public class SLL implements LinkedListADT {
     }
 
     @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public void delete(int index) throws IndexOutOfBoundsException {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index for delete is out of range");
+        }
+
+        if (index == 0) {
+            head = head.getNext();
+
+            if (head == null) {
+                tail = null;
+            }
+        } else {
+            Node current = head;
+            int count = 0;
+
+            while (count < index - 1) {
+                current = current.getNext();
+                count++;
+            }
+
+            if (index == size - 1) {
+                current.setNext(null);
+                tail = current;
+            } else {
+                current.setNext(current.getNext().getNext());
+            }
+        }
+
+        size--;
+    }
+
+    @Override
     public Object retrieve(int index) throws IndexOutOfBoundsException {
-        if(index < 0 || index >= size)
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index out of range");
+        }
 
         Node current = head;
         int count = 0;
 
-        while(count < index) {
+        while (count < index) {
             current = current.getNext();
             count++;
         }
@@ -119,10 +155,11 @@ public class SLL implements LinkedListADT {
         Node current = head;
         int index = 0;
 
-        while(current != null) {
-            if(current.getData().equals(data)) {
+        while (current != null) {
+            if (current.getData().equals(data)) {
                 return index;
             }
+
             current = current.getNext();
             index++;
         }
@@ -133,16 +170,5 @@ public class SLL implements LinkedListADT {
     @Override
     public boolean contains(Object data) {
         return indexOf(data) != -1;
-    }
-
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    @Override
-    public void insert(Object data, int index) throws IndexOutOfBoundsException {
-        throw new UnsupportedOperationException("Not implemented yet");
     }
 }
